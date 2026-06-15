12fd4fb682defe61638ddfd6bf6dc263fbeee2ff
RAHUL <rahuldurgapal555@gmail.com>
2026-06-15 19:03:55 +0530

    implemented order endpoints

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import type { Request, Response } from "express";
-import { Assets, PrismaClient, Side, Type } from "./generated/prisma/client";
+import { Assets, PrismaClient, Side, Status, Type } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import zod from "zod";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 import {authMiddleware} from "./middleware"
 import { use } from "react";
+import { id } from "zod/locales";
 
 const app = express();
 
@@ -32,6 +33,13 @@ const signinSchema = zod.object({
     password : zod.string()
 });
 
+const oderSchema = zod.object({
+    stockId : zod.string(),
+    side: zod.nativeEnum(Side),
+    type : zod.nativeEnum(Type),
+    price : zod.number().positive(),
+    quantity : zod.number().positive()
+});
 
 app.post("/signup" , async (req , res)=>{
     const parsed = signupSchema.safeParse(req.body);
@@ -155,6 +163,212 @@ app.get("/balance" , authMiddleware , async (req , res)=>{
 app.get("/stocks" , async (req , res)=>{
     const stocks = await prisma.stock.findMany();
     return res.json(stocks);
+})
+
+app.post("/order" , authMiddleware , async(req , res )=>{
+    const userId = req.userId;
+
+    if(!userId){
+        return res.status(401).json({
+            message : "unthauthorized user"
+        })
+    };
+
+    const parsed = oderSchema.safeParse(req.body);
+    if(!parsed.success){
+        return res.status(401).json({
+            error:parsed.error.issues
+        })
+    };
+
+    const {stockId , side , type , price ,  quantity }  = parsed.data
+
+    const stock = await prisma.stock.findUnique({
+        where:{
+            id : stockId
+        }
+    });
+
+    if(!stock){
+        return res.status(401).json({
+            message : "stock not found"
+        })
+    }
+
+   
+
+    const requiredAmount = price * quantity;
+
+    const amount = await prisma.balance.findFirst({
+        where:{
+            userId:req.userId,
+            asset:Assets.INR
+        },
+        select:{
+            available:true,
+            locked:true
+        }
+        
+    });
+
+    if(!amount){
+        return res.status(401).json({
+            message : "no amount"
+        })
+    }
+
+    console.log(amount.available);
+    console.log(typeof amount.available);
+
+    const availableAmount = Number(amount.available);
+
+
+    if(requiredAmount > availableAmount){
+        return res.status(401).json({
+            message : "insufficient balance"
+        })
+    }
+    const newLocked = Number(amount.locked)+ requiredAmount;
+
+    const updated =  await prisma.balance.update({
+        where: {
+            userId_asset:{
+                userId : req.userId!,
+                asset : Assets.INR
+            }
+        },
+        data:{
+            available : availableAmount - requiredAmount,
+            locked : newLocked
+        }
+    });
+
+    const createdOrder = await prisma.order.create({
+        data:{
+            userId,
+            stockId,
+            side,
+            type,
+            price,
+            quantity,
+            filledQuantity : 0,
+            status : Status.OPEN
+        }
+    })
+
+    return res.json({
+        orderId : createdOrder.id,
+        message : "order created successfully"
+    });
+
+    
+
+ 
+        
+
+    
+
+
+})
+
+app.get("/orders" , authMiddleware , async (req , res)=>{
+    const userId = req.userId;
+    
+    if(!userId){
+        return res.status(401).json({
+            message : "unauthorized user"
+        })
+    };
+
+    const orders = await prisma.order.findMany({
+        where:{
+            userId
+        },
+        orderBy:{
+            createdAt:"desc"
+        }
+    })
+
+    return res.json({
+        orders
+    })
+});
+
+app.delete("/order/:orderId" , authMiddleware , async (req , res)=>{
+
+    const userId = req.userId;
+    const orderId  = req.params.orderId
+
+    if(!userId){
+        return res.json({
+            message : "unauthorized user"
+        })
+    };
+
+    const order = await prisma.order.findFirst({
+        where : {
+            userId,
+            id : orderId
+        }
+    });
+
+    if(!order){
+        return res.status(401).json({
+            message : "no oder found"
+        })
+    };
+
+    if(
+        order.status != Status.OPEN && 
+        order.status != Status.PARTIALLY_FILLED
+    ) {
+        return res.status(401).json({
+            message : "order can't be cancelled"
+        })
+    }
+
+    const amountToUnlock = Number(order.price) * ( Number(order.quantity) - Number(order.filledQuantity) );
+    
+    const balance = await prisma.balance.findFirst({
+        where:{
+            userId ,
+            asset : Assets.INR
+        }
+    });
+
+    if(!balance){
+        return res.status(401).json({
+            message : "balance not found"
+        })
+    }
+
+    const updatedBalance = await prisma.balance.update({
+        where:{
+            userId_asset:{
+                userId ,
+                asset : Assets.INR
+            } 
+        },
+        data:{
+            available : Number(balance.available) + amountToUnlock,
+            locked : Number(balance.locked) - amountToUnlock
+        }
+    });
+
+    const cancelledOrder  = await prisma.order.update({
+        where:{
+            id : orderId
+        },
+        data:{
+            status : Status.CANCELLED 
+        }
+    })
+
+    return res.json({
+        updatedBalance,
+        cancelledOrder
+    });
+
 })
 
 app.listen(3000);
